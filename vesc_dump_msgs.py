77a813dd8e14ca643513816c21d2bd88627319c9
fcodes0 <fcodes0@proton.me>
2023-08-10 12:57:46 -0400

    implemented getDynoPower to return power output of dyno vesc

--- vesc_dump_msgs.py
+++ vesc_dump_msgs.py
@@ -10,6 +10,7 @@ parser.add_argument("--bitrate", default=1000000, type=int, help="CAN bit rate")
 parser.add_argument("--node-id", default=100, type=int, help="CAN node ID")
 parser.add_argument("--dna-server", action='store_true', default=False, help="run DNA server")
 parser.add_argument("port", default=None, type=str, help="serial port")
+parser.add_argument("--dyno-node-id",default=None,type=int,help="dyno node id")
 args = parser.parse_args()
 
 
@@ -26,7 +27,20 @@ if args.dna_server:
 #node.add_handler(None, lambda msg: print(dronecan.to_yaml(msg)))
 
 # callback for printing vesc data
-node.add_handler(dronecan.thirdparty.vesc.RTData, lambda msg: print(dronecan.to_yaml(msg)))
+#node.add_handler(dronecan.thirdparty.vesc.RTData, lambda msg: print(dronecan.to_yaml(msg)))
+#node.add_handler(dronecan.thirdparty.vesc.RTData, lambda msg: print(msg))
+def getDynoPower(event):
+    # print(args.dyno_node_id)
+    # print(type(event.transfer.source_node_id))
+    if event.transfer.source_node_id == args.dyno_node_id:
+        #print(dronecan.to_yaml(event))
+        dynopower = float(str(event.transfer.payload._fields["curr_in"]))*float(str(event.transfer.payload._fields["volt_in"]))
+        #print(f'Dyno Power:{dynopower}')
+        return 
+
+def get()
+
+node.add_handler(dronecan.thirdparty.vesc.RTData, getDynoPower)
 
 
 # Running the node until the application is terminated or until first error.
